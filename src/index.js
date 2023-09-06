c41e937a8e5368bca9d6cc68dbf480544c1918ac
Robbie Decker <robbd56@gmail.com>
2023-09-05 20:09:42 -0400

    Wrapping up the assets managament example

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import _ from 'lodash';
 import './style.css';
 import myName from './myName';
+import cat from './cat.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash is now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    // element.textContent = myName("Robbie");
-    element.classList.add('hello');
-    
-    return element;
-  }
+  const element = document.createElement('div');
+
+  // Lodash is now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.textContent = myName("Robbie");
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = cat;
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
+
+  return element;
+}
   
   document.body.appendChild(component());
