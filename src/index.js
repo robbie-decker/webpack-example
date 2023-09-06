47774f23bc4588264f736888b22b858c9973d0fb
Robbie Decker <robbd56@gmail.com>
2023-09-05 20:36:43 -0400

    Finishing output management

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import myName from './myName';
-import cat from './cat.jpg';
-import Data from './data.xml';
-import Notes from './data.csv';
+import printMe from './print.js';
 
-
-function component() {
+ function component() {
   const element = document.createElement('div');
-
-  // Lodash is now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // element.textContent = myName("Robbie");
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = cat;
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
-
-  return element;
-}
+  const btn = document.createElement('button');
   
-  document.body.appendChild(component());
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  
+  element.appendChild(btn);
+  
+  return element;
+ }
+ document.body.appendChild(component());
--- src/myName.js
+++
@@ -1,3 +0,0 @@
-const myName = (name) => "Hi! My Name is " + name;
-
-export default myName;
